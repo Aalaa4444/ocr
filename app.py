c6b72caa449eb3e9327d160554980a9d4dbdf2d8
Aalaa4444 <89411360+Aalaa4444@users.noreply.github.com>
2024-11-20 10:50:33 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+from ocr.ocr_model import *
+from ocr.google_drive import *
+from PIL import Image
+from ocr.prerocess import *
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload_invoice():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file provided"}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({"error": "No file selected"}), 400
+
+    print(f"File received: {file.filename}") 
+
+    file.save("uploaded_invoice.png")
+    print("File saved as 'uploaded_invoice.png'") 
+
+    try:
+        from PIL import Image
+        img = Image.open("uploaded_invoice.png")
+        print("Image opened successfully") 
+        j=extract_invoice_details(img)
+        jsonify({"message": "File uploaded successfully"})
+        
+        
+    except Exception as e:
+        print(f"Error opening image: {e}")
+        return jsonify({"error": "Failed to process image"}), 500
+
+    return jsonify(j), 200
+
+@app.route('/monitor-folder', methods=['GET'])
+def monitor_folder():
+    service = authenticate_drive()
+    folder_id = '1sHTeNQX_QTP-EooQkXiVV2VtZfttoX8f'
+    
+    downloaded_files = load_downloaded_files() 
+    jsonify({"message": "Monitoring folder triggered."}),200
+    
+    try:
+        while True:
+            jj= monitor(service, folder_id, downloaded_files)
+            print(jj)
+            save_downloaded_files(downloaded_files)
+            time.sleep(60)  # Poll every minute
+            jj= monitor(service, folder_id, downloaded_files)
+            save_downloaded_files(downloaded_files)
+            return jsonify(jj),200
+    except KeyboardInterrupt:
+        print("Stopping monitoring.")
+        save_downloaded_files(downloaded_files)
+        return jsonify(jj),200
+
+if __name__ == "__main__":
+    app.run(debug=True)
